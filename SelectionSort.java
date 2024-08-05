fd68b222388ceb720eeeb9fc0493ce0308be6728
Pragya Singh <129204388+pragyasingh-29@users.noreply.github.com>
2024-08-05 21:05:26 +0530

    Add files via upload

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,26 +1,33 @@
 public class SelectionSort{
+
     public static void selectionSort(int arr[]){
+        for(int i=0; i<arr.length-1;i++){
+            int minPos = i;
+            for(int j=i+1;j<arr.length;j++){
+               if(arr[minPos] < arr[j]){
 
-         for(int turn = 0; turn<arr.length-1;turn++){
-            int minPos = turn;
-            for(int j=turn+1; j<arr.length;j++){
-                if(arr[minPos] > arr[j]){
-                    minPos = j;
-                }
+                minPos = j;
+
+               }
             }
-            //swap
-            int temp = arr[minPos];
-            arr[minPos] = arr[turn];
-            arr[turn] = temp;
-         }
-
-         //print 
-         for(int i=0; i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-         }
+             //swap
+            int temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+        }
+        
     }
+    
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
     public static void main(String[]args){
         int arr[] = {5,4,1,3,2};
         selectionSort(arr);
+        printArray(arr);
     }
 }
+
